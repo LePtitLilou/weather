34f42629a14aced0ea01205da2cb4fdc1b975bc1
Charles Doutriaux <charles@leptitlilou.com>
2014-12-31 22:53:55 -0800

    ok got to record and plot from mic, need to understand length etc...

--- Test/test_read_stream.py
+++ Test/test_read_stream.py
@@ -1,14 +1,33 @@
 #Tests to read in stream 
+import pyaudio
+import matplotlib
+matplotlib.use("macosx")
+import matplotlib.pyplot as plt
+import struct
 
-for c1 in [512,2048,4096,8192,16384]:
-  for c2 in [512,2048,4096,8192,16384]:
-    stream = p.open(format = pyaudio.paInt16,channels=2,rate=44100,input=True,frames_per_buffer=c1)
+p = pyaudio.PyAudio()
+
+for i in range(p.get_device_count()):
+  print "Device:", i
+  print p.get_device_info_by_index(i)["name"]
+
+for c1 in [512,2048,4096,8192]:
+  print c1
+  for c2 in [512,2048,4096,8192]:
+    print "\t",c2
+    stream = p.open(format = pyaudio.paFloat32,channels=1,rate=44100,input=True,frames_per_buffer=c1)
     try:
+      st=""
+      l=0
       for i in range(0,int(44100/1024*2)):
         data = stream.read(c2)
-        pass
-      except:
-        print "boooooooo"
-      stream.stop_stream()
-      stream.close()
+        st+=data
+        l+=c2
+      data2 = struct.unpack("%if"%l,st)
+      plt.plot(data2)
+      plt.show()
+    except Exception,err:
+      print "boooooooo"
+    stream.stop_stream()
+    stream.close()
 
