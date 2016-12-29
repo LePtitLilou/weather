734c2d31b5a8202598dde8995587f9253d064a78
Charles Doutriaux <charles@leptitlilou.com>
2016-12-29 10:43:43 -0800

    Adding function to deal with weather underground

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-setup (name = "weather",
-       author="Charles Doutriaux"
-       version=0.9,
-       description = "Utilities to read back in my weather station data".
-       packages = ['weather'],
-       package_dir = {'weather': 'src'},
+setup (name = "lpl_weather",
+       author="Charles Doutriaux",
+       version="0.1",
+       description = "Utilities to read back in my weather station data",
+       packages = ['lpl_weather'],
+       package_dir = {'lpl_weather': 'src'},
        #data_files = [ ("share/weather",())],
       )
