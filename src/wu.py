46e9c453affc973d8237bbc6e8c31ad652342766
Charles Doutriaux <charles@leptitlilou.com>
2017-01-01 23:30:52 -0800

    loggly cookie useless, removed

--- src/wu.py
+++ src/wu.py
@@ -119,21 +119,21 @@ def month2cdms(year, month, variables=["temperature"], station=None):
     f.close()
 
 
-def get_history(year, month, day, station_id=None, pathout=None, session=None,
-                key="606f3f6977348613", loggly="25e68a9b-b928-4129-b536-cc3112ee71c3"):
+def get_history(year, month, day, station_id=None, pathout=None, session=None, key=None):
     if station_id is None:
         station_id = mystation(year)
+    if key is None:
+        key = "606f3f6977348613"
     date = "%.4i%.2i%.2i" % (year, month, day)
     if pathout is None:
         pathout= os.path.join(os.path.expanduser("~/"),"Weather","history")
     name = os.path.join(pathout,station_id, date+".json")
     f = open(name, "w")
-    cookies = {"logglytrackingsession": loggly}
     url = "https://api-ak.wunderground.com/api/%s/history_%s/units:english/v:2.0/q/pws:%s.json" % (
         key, date, station_id)
     if session is None:
         session = requests.Session()
-    r = session.get(url, cookies=cookies)
+    r = session.get(url)
     f.write(r.content)
     f.close()
     if not r.status_code == 200:
